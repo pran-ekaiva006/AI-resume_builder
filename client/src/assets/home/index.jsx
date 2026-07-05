01ff2c3a147f1d9d5083dd73fe55f01a5c66b80e
Pranjal Kumar Verma <pranjalverma975@gmail.com>
2026-07-05 15:26:44 +0530

    fix: reference error index is not defined in features map

--- client/src/assets/home/index.jsx
+++ client/src/assets/home/index.jsx
@@ -143,7 +143,7 @@ export default function Home() {
             </motion.div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {features.map((feature) => (
+              {features.map((feature, index) => (
                 <motion.div
                   key={feature.title}
                   initial={{ opacity: 0, y: 16 }}
